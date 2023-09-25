7ee549fe866515c913392140851d543ada066a63
paul-nguyen96 <paul121596@icloud.com>
2023-09-24 21:50:50 -0500

    quality check on transformation w/ soda

--- dags/retail.py
+++ dags/retail.py
@@ -71,5 +71,13 @@ def retail():
             select=['path:models/transform']
         )
     )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+    check_transform()
 # airflow tasks test retail {method} 2023-01-01
 retail()
